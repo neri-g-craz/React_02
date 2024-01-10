d76c7014f0a62604b1d73b3a8fdf493eb9dc1970
neri-g-craz <re.gomezor@gmail.com>
2024-01-09 20:54:23 -0600

    (+) React: Componentes, fragmentos y props (no modules must use -npm install-)

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import './Button.css'
+
+/* 
+* Props. Propiedades que se pueden asignar a los componentes para modificar el texto, la informacion desde la renderización del mismo.
+*Se asignan como parámetro de la función con el nombre del prop y debe ir entre llaves.
+*Se manda a llamar en el bloque de código donde se debe implementar utilizando llaves
+*/
+
+const Button = ({label}) => {
+    return (
+        <>
+        <button>
+            {label}
+        </button>
+        </>
+    )
+}
+
+export default Button
